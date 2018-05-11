0219800c60b4d5d478929fb0777ecc133ae77765
HawkCorrigan <dgroelle@siemens.md.st.schule.de>
2018-05-12 01:03:38 +0200

    Actually add the files

---
+++ udpSender.c
@@ -0,0 +1,37 @@
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/uio.h>
+
+
+void startSender(){
+    const char* host=0; /* localhost */
+    const char* port="daytime";
+    struct addrinfo hints;
+    memset(&hints,0,sizeof(hints));
+    hints.ai_family=AF_UNSPEC;
+    hints.ai_socktype=SOCK_DGRAM;
+    hints.ai_protocol=0;
+    hints.ai_flags=AI_ADDRCONFIG;
+    struct addrinfo* res=0;
+    int err=getaddrinfo(host,port,&hints,&res);
+    if (err!=0) {
+        exit(1);
+    }
+    
+    int fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    if(fd==-1){
+        exit(1);
+    }
+    char* content = "Teststuff\0";
+    size_t size=10;
+    if (sendto(fd,content,size,0,
+    res->ai_addr,res->ai_addrlen)==-1) {
+        exit(1);
+    }
+}
