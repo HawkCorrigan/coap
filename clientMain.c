5d2caf5b5ec46e2c77e6b00a42361642589c5495
Malik Mann <mm.malik.mann@gmail.com>
2018-05-28 17:01:30 +0200

    Added very basic CLI. (broken)

--- clientMain.c
+++ clientMain.c
@@ -1,11 +1,90 @@
 #include <stdint.h>
+#include <unistd.h>
 
 #include "coapMessage.h"
 #include "udpSender.h"
 
-
 int main(int argc, char const *argv[])
 {
-    startSender();
+    if (argc == 1)
+    {
+        usage(argv[0]);
+        exit(1);
+    }
+
+    const char *host = "localhost";
+    const char *port = "5683";
+    const char *payload = NULL;
+    coap_message_t *message = malloc(sizeof(coap_message_t));
+    message->header = malloc(sizeof(coap_header_t));
+    int opt;
+    while ((opt = getopt(argc, argv, "Ne:f:h:m:o:p:O:T:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'e':
+            message->payload.len = sizeof(optarg);
+            message->payload.p = malloc(message->payload.len);
+            memcpy(message->payload.p, optarg, message->payload.len);
+            break;
+        case 'f':
+            //TODO
+            break;
+        case 'h':
+            host = optarg;
+            break;
+        case 'm':
+            //TODO
+            break;
+        case 'o':
+            //TODO
+            break;
+        case 'O':
+            //TODO
+            break;
+        case 'p':
+            port = optarg;
+            break;
+        case 'T':
+            message->token.len = sizeof(optarg);
+            message->token.p = malloc(message->token.len);
+            memcpy(message->token.p, optarg, message->token.len);
+            break;
+        case 'N':
+            //TODO
+            break;
+        default:
+            usage(argv[0]);
+            exit(1);
+        }
+    }
+    uint8_t * package;
+    build(package, buflen, message);
+    startSender(host, port, package, buflen);
     return 0;
+}
+
+void usage(const char *program)
+{
+    const char *p;
+
+    p = strrchr(program, '/');
+    if (p)
+        program = ++p;
+
+    fprintf(stderr,
+            "usage: %s [OPTIONS] URI\n\n"
+            "\tURI can be an absolute or relative coap URI,\n"
+            //"\t-a addr\tthe local interface address to use\n"
+            "\t-e text\t\tinclude text as payload (use percent-encoding for\n"
+            "\t\t\tnon-ASCII characters)\n"
+            "\t-f file\t\tfile to send with PUT/POST (use '-' for STDIN)\n"
+            "\t-h host\n"
+            "\t-m method\trequest method (get|put|post|delete|fetch|patch|ipatch), default is 'get'\n"
+            "\t-N\t\tsend NON-confirmable message\n"
+            "\t-o file\t\toutput received data to this file (use '-' for STDOUT)\n"
+            "\t-p port\n"
+            "\t-O num,text\tadd option num with contents text to request\n"
+            "\t-T token\tinclude specified token\n",
+            program);
 }
--- udpSender.c
+++ udpSender.c
@@ -9,49 +9,31 @@
 
 #include "coapMessage.h"
 
-
-void startSender(){
-    const char* host="8.8.8.8"; /* localhost */
-    const char* port="5683";
+void startSender(const char *host, const char *port, const char *message)
+{
     struct addrinfo hints;
-    memset(&hints,0,sizeof(hints));
-    hints.ai_family=AF_UNSPEC;
-    hints.ai_socktype=SOCK_DGRAM;
-    hints.ai_protocol=0;
-    hints.ai_flags=AI_ADDRCONFIG;
-    struct addrinfo* res=0;
-    int err=getaddrinfo(host,port,&hints,&res);
-    if (err!=0) {
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = 0;
+    hints.ai_flags = AI_ADDRCONFIG;
+    struct addrinfo *res = 0;
+    int err = getaddrinfo(host, port, &hints, &res);
+    if (err != 0)
+    {
         exit(1);
     }
-    
+
     int fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
-    if(fd==-1){
+    if (fd == -1)
+    {
         exit(1);
     }
-
-
-    size_t *size = malloc(sizeof(size_t)); 
-    char *package = malloc(sizeof(char)); 
-    coap_message_t *msg = malloc(sizeof(coap_message_t)); 
-    msg->header=malloc(sizeof(coap_header_t));
-    msg->header->vers=1;
-    msg->header->type=0;
-    msg->header->token_len=1;
-    msg->header->code_type=1;
-    msg->header->message_id=29546;
-    msg->token.len=1;
-    msg->token.p=malloc(1 * sizeof(uint8_t));
-    msg->token.p[0]=5;
-    msg->numopts=0;
-    msg->payload.len=0;
-    msg->payload.p=NULL;
-    build(package, size, msg);
-
-    printf("Sender: %d\n", *size);
-    printf("%d\n", *package);
-    if (sendto(fd, package, *size, 0, res->ai_addr,res->ai_addrlen) ==-1) {
+    char* content = "Teststuff\0";
+    size_t size=10;
+    if (sendto(fd,content,size,0,
+    res->ai_addr,res->ai_addrlen)==-1) {
         exit(1);
     }
 }
